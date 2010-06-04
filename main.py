3ee9214d89e714fd954cf0b57b034275b308e775
Ghansah Emmanuel <eghansah@gmail.com>
2010-06-04 15:00:25 +0000

    911ghana can now accept logs

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import cgi
 
+import pages
+
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-class call(db.Model):
+class Call(db.Model):
 	caller = db.StringProperty()
 	phone_number = db.StringProperty()
 	location = db.StringProperty()
-    date = db.DateTimeProperty(auto_now_add=True)
-	call_operator = db.UserProperty()
+	complaint = db.StringProperty()
+	date = db.DateTimeProperty(auto_now_add=True)
+	#call_operator = db.UserProperty()
 
 
-class police_officer(db.Model):
+class Police_officer(db.Model):
 	#Although this could be improved by splitting the officer and officer_locations,
 	#for now I don't c much benefit in doing it the right way.
 	#Of course, we could always improve it later
@@ -22,12 +25,38 @@ class police_officer(db.Model):
 	date_and_time = db.DateTimeProperty(auto_now_add=True)
 
 
+
+class MainPage(webapp.RequestHandler):
+	def get(self):
+		self.response.out.write( pages.index % {'msg':''} )
+	
+	def post(self):
+		#Simply post data into datastore.
+		#and confirm post
+		
+		call = Call()
+		call.caller = self.request.get('caller')
+		call.phone_number = self.request.get('phone-number')
+		call.complaint = self.request.get('complaint')
+		call.location = self.request.get('location')
+		#call.call_operator = db.UserProperty()
+		
+		
+		try:
+			call.put()
+			self.response.out.write( pages.index % {'msg':'Complaint from <b>%s</b> logged' % call.caller} )
+		except:
+			self.response.out.write( pages.index % {'msg':'System error.'} )
+
+
+
 application = webapp.WSGIApplication(
-                                     [('/', MainPage),
-                                      ('/sign', Guestbook),
-									  ('/logout', Logout)],
+                                     [
+										('/', MainPage),
+									 ],
                                      debug=True)
 
+	
 def main():
 	run_wsgi_app(application)
 
---
+++ pages.py
@@ -0,0 +1,24 @@
+
+
+
+index = """
+
+	<html>
+
+		<body>
+			%(msg)s <p />
+			<form name="call-log" method="post">
+				Caller <br>
+				<input name="caller" type="text"><br />
+				phone number<br />
+				<input name="phone-number" type="text"><br />
+				complaint <br />
+				<textarea name="complaint"></textarea><br />
+				location<br />
+				<input name="location" type="text"><br />
+				<input name="log-call" type="submit" value="log call"><br />
+		</body>
+	</html>
+
+
+"""
