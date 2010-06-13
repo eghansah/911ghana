012915e77eadb4b4ef4cbdd7aec934014ae6abc1
Ghansah Emmanuel <eghansah@gmail.com>
2010-06-13 12:23:54 +0000

    Version 1.0 Beta

--- main.py
+++ main.py
@@ -1,26 +1,78 @@
 import cgi
 
+import key
 import pages
+import urllib
+
+import datetime
+import PyGeoRSS
+from cStringIO import StringIO
+
+from geo.geomodel import GeoModel
+from BeautifulSoup import BeautifulSoup
 
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-class Call(db.Model):
+
+
+
+
+def getGeoCode( town ):
+	
+	lat = lng = None
+	
+	try:
+		url = "http://maps.google.com/maps/api/geocode/xml?%s"
+		params = urllib.urlencode({
+					'address': town,
+					'sensor':'false'	
+		})
+
+	
+		f = urllib.urlopen( url % params )
+		xml = f.read()
+		f.close()
+	
+		soup = BeautifulSoup(xml)
+		loc = soup('location')[0]
+		lat = loc.lat.text
+		lng = loc.lng.text
+	except:
+		pass
+	return (lat, lng)
+
+
+
+def getNearestOfficers( loc ):
+	results = Police_officer_loc.proximity_fetch(
+			Police_officer_loc.all(),
+			loc,  # Or db.GeoPt
+			max_results=10,
+			max_distance=80467)
+	
+	
+	return [officer.name for officer in results]
+
+
+
+class Call(GeoModel):
 	caller = db.StringProperty()
 	phone_number = db.StringProperty()
-	location = db.StringProperty()
+	location_name = db.StringProperty()
+	#geoloc = db.GeoPtProperty()
 	complaint = db.StringProperty()
 	date = db.DateTimeProperty(auto_now_add=True)
 	#call_operator = db.UserProperty()
 
 
-class Police_officer(db.Model):
+class Police_officer_loc(GeoModel):
 	#Although this could be improved by splitting the officer and officer_locations,
 	#for now I don't c much benefit in doing it the right way.
 	#Of course, we could always improve it later
-	officer = db.StringProperty()
+	name = db.StringProperty()
 	last_known_location = db.StringProperty()
 	date_and_time = db.DateTimeProperty(auto_now_add=True)
 
@@ -34,25 +86,117 @@ class MainPage(webapp.RequestHandler):
 		#Simply post data into datastore.
 		#and confirm post
 		
-		call = Call()
+		call = Call( location=db.GeoPt(0, 0) )
 		call.caller = self.request.get('caller')
 		call.phone_number = self.request.get('phone-number')
 		call.complaint = self.request.get('complaint')
-		call.location = self.request.get('location')
-		#call.call_operator = db.UserProperty()
+		call.location_name = "%s, ghana" % self.request.get('location')
 		
-		
+		lat, lng = getGeoCode( call.location_name )
+		loc = "Unknown"
+		if lat or lng:
+			call.location = db.GeoPt(lat, lng)
+			call.update_location()
+			loc = (lat, lng)
+
+
 		try:
 			call.put()
-			self.response.out.write( pages.index % {'msg':'Complaint from <b>%s</b> logged' % call.caller} )
+			context = {
+						'msg':'Complaint from <b>%s</b> logged.<br />Officers notified: %s' % (call.caller, getNearestOfficers(call.location)) }
+						#'msg':'Complaint from <b>%s</b> logged.<br />Officers notified: %s' % (call.caller, loc) }
+			self.response.out.write( pages.index % context )
 		except:
 			self.response.out.write( pages.index % {'msg':'System error.'} )
+
+
+
+
+
+
+
+class UpdateOfficerLocPage(webapp.RequestHandler):
+	def get(self):
+		self.response.out.write( pages.officer_loc % {'msg':''} )
+	
+	def post(self):
+		#Simply post data into datastore.
+		#and confirm post
+		
+		officer = Police_officer_loc( location=db.GeoPt(0, 0) )
+		officer.name = self.request.get('officer')
+		officer.last_known_location = "%s, ghana" % self.request.get('last_known_location')
+		
+		lat, lng = getGeoCode( officer.last_known_location )
+		loc = "Unknown"
+		if lat or lng:
+			officer.location = db.GeoPt(lat, lng)
+			officer.update_location()
+			loc = (lat, lng)
+
+		try:
+			officer.put()
+			context = {
+						'msg':'Officer %s\'s location has been updated.' % officer.name }
+						#'msg':'Complaint from <b>%s</b> logged.<br />Officers notified: %s' % (call.caller, loc) }
+			self.response.out.write( pages.officer_loc % context )
+		except:
+			self.response.out.write( pages.officer_loc % {'msg':'System error.'} )
+
+
+
+
+
+
+class RssCrime(webapp.RequestHandler):
+	def get(self):
+		
+		crimes = Call.all()
+		list_of_reported_crimes = []
+		i = 0
+
+		for crime in crimes:
+			i += 1
+			list_of_reported_crimes.append(
+				PyGeoRSS.RSSItem(
+					title = crime.caller,
+					link = "http://911ghana.appspot.com/",
+					description = crime.complaint,
+					pubDate = crime.date,
+					guid = PyGeoRSS.Guid("http://911ghana.appspot.com/%s" % str(i) ),
+					geo_rss_pt = PyGeoRSS.GeoRssPoint(crime.location.lat, crime.location.lon),
+					#location = PyGeoRSS.GeoRssPoint(0, 0),
+				)
+			)
+			
+		
+		rss = PyGeoRSS.RSS2(
+			title = "911Ghana Crime feed",
+			link = "http://911ghana.appspot.com/rsscrime",
+			description = "Realtime crime feed.",
+			lastBuildDate = datetime.datetime.now(),
+
+			items = list_of_reported_crimes)
+
+		rss_response = StringIO()
+		rss.write_xml(rss_response)
+		d = datetime.datetime.now()
+		
+		self.response.headers["Content-Type"] = "application/rss+xml"
+		self.response.headers.add_header("Expires", d.strftime("%a, %d %b %Y %H:%M:%S GMT"))
+		self.response.out.write(rss.to_xml())
+
+		rss_response.close()
+		
+
 
 
 
 application = webapp.WSGIApplication(
                                      [
 										('/', MainPage),
+										('/rsscrime.*', RssCrime),
+										('/update_location', UpdateOfficerLocPage),
 									 ],
                                      debug=True)
 
--- pages.py
+++ pages.py
@@ -5,18 +5,88 @@ index = """
 
 	<html>
 
+
+		<head>
+		<!-- <meta name="viewport" content="initial-scale=1.0, user-scalable=no" /> -->
+		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+		<script type="text/javascript">
+		  function initialize() {
+			var latlng = new google.maps.LatLng(5.571908,-0.207367);
+			var myOptions = {
+			  zoom: 13,
+			  center: latlng,
+			  mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+/*
+			var marker = new google.maps.Marker({
+				  position: new google.maps.LatLng(5.571908,-0.207367), 
+				  map: map,
+				  title:"Hello World!"
+			  });
+*/
+			
+			var georssLayer = new google.maps.KmlLayer("http://911ghana.appspot.com/rsscrime");
+			georssLayer.setMap(map);
+		  }
+			
+
+			
+		</script>
+		</head>
+
+
+		<body onload="initialize()">
+		
+		<table width=100%% height=100%% >
+			<tr>
+				<td width=20%% valign=top>
+					%(msg)s <p>
+					<form name="call-log" method="post">
+						Caller <br>
+						<input name="caller" type="text"><br />
+						phone number<br />
+						<input name="phone-number" type="text"><br />
+						complaint <br />
+						<textarea name="complaint"></textarea><br />
+						location<br />
+						<input name="location" type="text"><br />
+						<input name="log-call" type="submit" value="log call"><br />
+					</form>
+					</p>
+
+					<p>
+					Instructions for use:<br />
+					/ ------------------ Please use this url for logging customer calls <br />
+					/update_location --- Please use this to update officer locations
+					</p>
+				</td>
+				<td>
+					<div id="map_canvas" style="width:100%%; height:100%%"></div>			
+				</td>
+			</tr>
+		</table>
+
+		</body>
+	</html>
+
+"""
+
+
+
+officer_loc = """
+
+	<html>
+
 		<body>
 			%(msg)s <p />
 			<form name="call-log" method="post">
-				Caller <br>
-				<input name="caller" type="text"><br />
-				phone number<br />
-				<input name="phone-number" type="text"><br />
-				complaint <br />
-				<textarea name="complaint"></textarea><br />
-				location<br />
-				<input name="location" type="text"><br />
-				<input name="log-call" type="submit" value="log call"><br />
+				Officer <br>
+				<input name="officer" type="text"><br />
+				Last known location<br />
+				<input name="last_known_location" type="text"><br />
+				<input name="update-loc" type="submit" value="update loc"><br />
 		</body>
 	</html>
 
